562bde04701807bfc6fdecd5d3c9a70218767205
Darshan Sonawane <sonawanedarshan35@gmail.com>
2026-08-13 20:23:48 +0530

    feat: initialize employee-compensation-service project with basic Maven configuration and main application class

---
+++ src/main/java/com/employee/compensation/Application.java
@@ -0,0 +1,8 @@
+package com.employee.compensation;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Employee Compensation Service Started");
+    }
+}
